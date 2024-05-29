11fa1c7cba0a4c0ecc2394cf64265746cd892770
hanhmy <91711323+myhanh29@users.noreply.github.com>
2024-05-29 22:58:31 +0200

    Benutzerliste nach dem Speichern der Benutzerdaten anzeigen

--- source/repos/Ausgabenkontrolle/Ausgabenkontrolle/Form1.cs
+++ source/repos/Ausgabenkontrolle/Ausgabenkontrolle/Form1.cs
@@ -56,13 +56,13 @@ namespace Ausgabenkontrolle
             button1.Text = "Save";
             button1.Dock = DockStyle.Bottom;
             form.Controls.Add(button1);
-            button1.Click += (s, eventArgs) => { Button1_Click(s, eventArgs, form); };
+            button1.Click += (s, eventArgs) => { SaveButonnClick(s, eventArgs, form); };
 
             Button button2 = new Button();
             button2.Text = "Cancel";
             button2.Dock = DockStyle.Bottom;
             form.Controls.Add(button2);
-            button2.Click += Button2_Click; 
+            button2.Click += CancelButonnClick; 
             form.ShowDialog();
         }
         // Methode zum Speichern der Daten in der Datenbank
@@ -109,47 +109,56 @@ namespace Ausgabenkontrolle
             }
         } 
         // Ereignishandler für den Klick auf den Speichern-Button
-        private void Button1_Click(object sender, EventArgs e, Form form)
+        private void SaveButonnClick(object sender, EventArgs e, Form form)
         {
             SavetoDB(sender, e);
-           SaveFileDialog svg = new SaveFileDialog()
-            {
-                Filter = "PDF files (*.pdf)|*.pdf"
-            };
-            
-               if (svg.ShowDialog() == DialogResult.OK)
-            {
-                using (FileStream stream = new FileStream(svg.FileName, FileMode.Create))
-                {
-                    Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
-                    PdfWriter.GetInstance(pdfDoc, stream);
-                    pdfDoc.Open();
-                    foreach (Control control in form.Controls)
-                    {
-                        if (control is UserControl1 userControl)
-                        {
-                            var (labels, datas) = userControl.GetContent();
-                            PdfPTable t = new PdfPTable(2);
-                            
-                            t.DefaultCell.PaddingBottom = 10;
-                            for (int i = 0; i < labels.Length; i++)
-                            {
-                                t.AddCell(new PdfPCell(new Phrase(labels[i])){ Border = PdfPCell.BOX });
-                                t.AddCell(new PdfPCell(new Phrase(datas[i])){ Border = PdfPCell.BOX });
+   
+            form.Hide();
+            Form2 form2 = new Form2();
+            form2.Show();
+            this.Hide();
+            this.Close();
+            /*  SaveFileDialog svg = new SaveFileDialog()
+               {
+                   Filter = "PDF files (*.pdf)|*.pdf"
+               };
 
-                            }
-                            pdfDoc.Add(t);
-                        }
-                    }
-                    pdfDoc.Close();
-                    stream.Close();
-                    Close();
+                  if (svg.ShowDialog() == DialogResult.OK)
+               {
+                   using (FileStream stream = new FileStream(svg.FileName, FileMode.Create))
+                   {
+                       Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
+                       PdfWriter.GetInstance(pdfDoc, stream);
+                       pdfDoc.Open();
+                       foreach (Control control in form.Controls)
+                       {
+                           if (control is UserControl1 userControl)
+                           {
+                               var (labels, datas) = userControl.GetContent();
+                               PdfPTable t = new PdfPTable(2);
 
-                }
-            }
+                               t.DefaultCell.PaddingBottom = 10;
+                               for (int i = 0; i < labels.Length; i++)
+                               {
+                                   t.AddCell(new PdfPCell(new Phrase(labels[i])){ Border = PdfPCell.BOX });
+                                   t.AddCell(new PdfPCell(new Phrase(datas[i])){ Border = PdfPCell.BOX });
+
+                               }
+                               pdfDoc.Add(t);
+                           }
+                       }
+                       pdfDoc.Close();
+                       stream.Close();*/
+
+
+
+
         }
+
+      
+
         // Ereignishandler für den Klick auf den Abbrechen-Button
-        private void Button2_Click(object sender, EventArgs e)
+        private void CancelButonnClick(object sender, EventArgs e)
         {
             Close();
         }
